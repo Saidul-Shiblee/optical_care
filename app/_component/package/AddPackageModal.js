76a3bb6264fc1215511eb0eaf58345ba96a9de68
Saidul-Shiblee <msi.shiblee@gmail.com>
2024-01-08 01:54:57 +0600

    commit as on 08.01.2024

--- app/_component/package/AddPackageModal.js
+++ app/_component/package/AddPackageModal.js
@@ -8,8 +8,8 @@ import {
     DialogTitle,
 
 } from "@/components/ui/dialog"
-import { Input } from "@/components/ui/Input"
-import { Label } from "@/components/ui/Label"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 import { toast } from "@/components/ui/use-toast"
 import { useEffect, useState } from "react"
 // show = { showAddPackageModal } setShow = { setShowAddPackageModal } packages = { packages } setPackages = { setPackages } pack = { pack }
